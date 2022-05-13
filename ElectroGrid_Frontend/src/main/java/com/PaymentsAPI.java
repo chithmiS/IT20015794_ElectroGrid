82afc66bb83e3f3c42dfcdee1996cb2727b98f5b
chithmi <chithmi99suhara@gmail.com>
2022-05-13 23:14:07 +0530

    Implement getParasMap method

--- ElectroGrid_Frontend/src/main/java/com/PaymentsAPI.java
+++ ElectroGrid_Frontend/src/main/java/com/PaymentsAPI.java
@@ -6,6 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map; 
+import java.util.HashMap; 
+import java.util.Scanner;
 
 /**
  * Servlet implementation class PaymentsAPI
@@ -51,6 +54,7 @@ public class PaymentsAPI extends HttpServlet {
 	 */
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
 	}
 
 	/**
@@ -59,5 +63,28 @@ public class PaymentsAPI extends HttpServlet {
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
+	
+	// Convert request parameters to a Map
+	private static Map getParasMap(HttpServletRequest request) 
+		{ 
+		 Map<String, String> map = new HashMap<String, String>(); 
+		try
+		 { 
+			 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8"); 
+			 String queryString = scanner.hasNext() ? 
+			 scanner.useDelimiter("\\A").next() : ""; 
+			 scanner.close(); 
+			 String[] params = queryString.split("&"); 
+		for (String param : params) 
+		 {
+			 String[] p = param.split("=");
+			 map.put(p[0], p[1]); 
+		 } 
+		 } 
+		catch (Exception e) 
+		 { 
+		 } 
+		return map; 
+		}
 
 }
